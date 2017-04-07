a0e3d429911ba54b04215bc84e71b01f7e6f0204
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2017-04-07 12:39:08 +0900

    台とボールの作成

---
+++ Assets/CueBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CueBall : MonoBehaviour {
+
+	private float speed = -300.0f;
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody> ().AddForce (
+			transform.right * speed,
+			ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
