9bc33cff3e47a62f8fa787d2da35500df3d2bbab
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2017-04-11 18:06:33 +0900

    簡易的たキューを実装

---
+++ Assets/Cue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cue : MonoBehaviour {
+
+	private float speed = -300.0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Return)) {
+			this.GetComponent<Rigidbody> ().AddForce (
+				transform.up * speed,
+				ForceMode.VelocityChange);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if ( collision.gameObject.tag == "Ball" ) {
+			Destroy(this.gameObject);
+		}
+	}
+}
--- Assets/CueBall.cs
+++ Assets/CueBall.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class CueBall : MonoBehaviour {
 
-	private float speed = -300.0f;
 	// Use this for initialization
 	void Start () {
-		this.GetComponent<Rigidbody> ().AddForce (
-			transform.right * speed,
-			ForceMode.VelocityChange);
+
 	}
 	
 	// Update is called once per frame
