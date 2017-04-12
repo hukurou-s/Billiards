50f52ec65548787aacefa4b1b6c747942d8582fb
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2017-04-12 16:03:35 +0900

    ボールがポケットに入った時の通知の追加

---
+++ Assets/Pocket.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	private GameObject Child;
+
+	// Use this for initialization
+	void Start () {
+		Child = transform.FindChild ("Sphere").gameObject;
+		Child.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if ( collision.gameObject.tag == "Ball" ) {
+			Child.gameObject.SetActive (true);
+		}
+	}
+}
